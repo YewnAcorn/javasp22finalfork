acd8bc43f48dadde28bda56ccaeeb6de4e934163
YewnAcorn <99105029+YewnAcorn@users.noreply.github.com>
2022-05-31 12:36:27 -0700

    a class that has the story in it

---
+++ Stroy.java
@@ -0,0 +1,98 @@
+package monsterdungeon;
+
+public class Story
+
+int numStoryGems = 0; // needs some method for seeing how many story gems the player has
+
+switch(numStoryGems){
+
+  case 0: //call to adventure
+
+
+  System.out.println("..."); System.out.println("...");
+  System.out.println("Digging, digging blindly in the dark");
+  System.out.println("You have to reach your cave-town and warn your cave-mates that an attack is immanent");
+  System.out.println("You almost reach the cave when you smell a terrible monster ahead of you!");
+  System.out.println("It attacks!");
+  System.out.println("You are saved in the last moment by another monster which attacks the first");
+  System.out.println("They cling together like two coiled serpents and and writhe in their death grips.");
+  System.out.println("...");
+  System.out.println("They both shreik and destroy each other leaving behind some kind of GEM");
+  System.out.println("...");
+  System.out.println("You pick up the GEM and are imbued with energy and strange powers! Suddenly you can see the tunnel you are in!");
+  System.out.println("You are surrounded by corpses of your fallen cave-mates!");
+  System.out.println("...");
+  System.out.println("These monsters must have killed all your cave-mates...");
+  System.out.println("...");
+  System.out.println("Another monster notices you and approaches!");
+  System.out.println("...");
+  System.out.println("It attacks!");
+  break;
+  //The player fights some monsters for a while until they get another story gem
+
+  case 1:
+
+  System.out.println("The wreckage of your foes lies behind you");
+  System.out.println("...");
+  System.out.println("You have destroyed all the monsters in what was once your cave-town");
+  System.out.println("After destroying the last monster. You find another GEM");
+  System.out.println("It begins to glow in the dark, and emits a sorcerous green light;");
+  System.out.println("As it floats in the air it illuminates the ruins of your cave-town:");
+  System.out.println("Your home, underground chamber with high ceilings and sweet air, now a smoldering pit of despair");
+  System.out.println("The green gem speaks:");
+  System.out.println("\"If you wish to save your people you must follow me!\"");
+  System.out.println("The gem begins to float away...");
+  System.out.println("You follow after, asking: ");
+  System.out.println("\"How can I save them if they are already all destroyed?\"");
+  System.out.println("GEM: \"You must go back in time and save them, of course");
+  System.out.println("YOU: \"How can I do that?\" ");
+  System.out.println("GEM: \"You must seek CRON in the CAVES OF DOOM\"");
+  System.out.println("\"Only he has the power of TIME\"");
+  System.out.println("You follow the gem to the tunnel deeper into the earth, the bottom exit of cave-town");
+  System.out.println("Behold the entrance to the CAVES of DOOM!");
+  System.out.println("You enter");
+  System.out.println("A monster attacks!");
+
+
+  break;
+  //player fights more monsters until they get another story gem
+  case 2:
+
+  System.out.println("Your guide GEM begins to speak again");
+  System.out.println("\"you have fought your way to the chamber of Cron\"");
+  System.out.println("\"You have defeated many foes and gained enough power.\"");
+  System.out.println("\"Approach Cron and make your request\"");
+  System.out.println("...");
+  Syste.out.println("You enter the final chamber. It is large and magnificient and ancient. You can see the whole world is connected through this cave");
+  System.out.println("\"Not just the whole world but all of TIME!\" shouts a demonic voice");
+  System.out.println("\"IT IS I, CRON THE TIME GOD! WHAT DO YOU SEEK?\"");
+  System.out.println("You: \"Will you turn back time and let me save my village from ultimate destruction?\"");
+  System.out.println("Cron: \"You think you can arrange time better than me? We'll see about that! Minions, attack this fool!\"");
+  System.out.println("A large army of demons surrounds you!");
+  System.out.println("They attack!");
+
+  break;
+  //player fights yet more monsters until they get yet another story gem
+  case 3:
+
+  System.out.println("Cron: \"...\"");
+  System.out.println("\"You have defeated my demonic army\"");
+  System.out.println("\"I will grant your wish and send you back in time\"");
+  System.out.println("\"Good luck saving your people!\"");
+  System.out.println("Green time energy begins to spiral around you.");
+  System.out.println("You feel yourself going back in time. A weird feeling");
+  System.out.println("Suddenly you are back at your village, but now it is not destroyed");
+  System.out.println("But it is under attack!");
+
+
+  break;
+  //once again the player.. fights monsters! until at last, they get the final story gem:
+
+  case 4:
+
+  System.out.println("You saved us " + player.getName() + "!");
+  System.out.println("Thank goodness you arrived in time");
+  System.out.println("Yes in *time* indeed!");
+  System.out.println("THE END");
+
+}
